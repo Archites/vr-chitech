030b42f97d0c98b119fa927601ca38670235a6a0
babestvl <babestvl@gmail.com>
2018-12-11 15:45:34 +0700

    Add common setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,47 +5,48 @@ const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-    ],
-  },
-  plugins: [
-    new LodashModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/common/Template/template.html'),
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.png$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							mimetype: 'image/png',
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'node_modules'),
+			path.resolve(__dirname, 'src'),
+		],
+		extensions: ['.js', '.jsx'],
+	},
+	plugins: [
+		new LodashModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, 'src/common/Template/template.html'),
+		}),
+	],
+	devServer: {
+		historyApiFallback: true,
+	},
 }
 
 module.exports = config
