03074df1aef952d3815e2ff10f9826eefb5c8566
babestvl <babestvl@gmail.com>
2019-01-29 10:57:07 +0700

    Change variable name

--- src/pages/ExamplePage/index.js
+++ src/pages/ExamplePage/index.js
@@ -11,9 +11,9 @@ class ExamplePage extends PureComponent {
 	handleOnClick = e => {
 		e.preventDefault()
 		const camera = this.mainCamera.current
-		const angle = camera.getAttribute('rotation')
-		const x = 0.1 * Math.cos((angle.y * Math.PI) / 180)
-		const y = 0.1 * Math.sin((angle.y * Math.PI) / 180)
+		const rotation = camera.getAttribute('rotation')
+		const x = 0.1 * Math.cos((rotation.y * Math.PI) / 180)
+		const y = 0.1 * Math.sin((rotation.y * Math.PI) / 180)
 		const pos = camera.getAttribute('position')
 		pos.x -= y
 		pos.z -= x
@@ -22,10 +22,7 @@ class ExamplePage extends PureComponent {
 	render() {
 		return (
 			<a-scene>
-				<WalkButton type="button" onClick={this.handleOnClick}>
-					WALK
-
-</WalkButton>
+				<WalkButton onClick={this.handleOnClick}>WALK</WalkButton>
 				<a-camera ref={this.mainCamera} id="main">
 					<a-cursor />
 				</a-camera>
@@ -33,10 +30,6 @@ class ExamplePage extends PureComponent {
 					io3d-furniture="id:10344b13-d981-47a0-90ac-f048ee2780a6"
 					position="-2 0 -3.2"
 					rotation="0 180 0"
-				/>
-				<a-entity
-					io3d-data3d="key:/3f995099-d624-4c8e-ab6b-1fd5e3799173/170515-0913-4p3ktf/1e588a3b-90ac-4a32-b5b8-ff2fda7f87c4.gz.data3d.buffer"
-					position="0 0 0"
 				/>
 			</a-scene>
 		)
