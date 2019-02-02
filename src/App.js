16ed5aa0704e7c35234ec5a4f8b73fa2eb4d24ec
narongsakjob <narongsak.chobsri@gmail.com>
2019-02-02 17:44:16 +0700

    Add link to inspector button

--- src/App.js
+++ src/App.js
@@ -3,83 +3,35 @@
 import React, { Component, Fragment } from 'react'
 // import { Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
 
 import ExamplePage from 'pages/ExamplePage'
 
-// const state = {
-// 	status: 'inpector'
-// }
-
-// const setStatus = () => {
-// 	if (state.status === 'inspector') {
-// 		state.status = 'view'
-// 	} else {
-// 		state.status = 'inspector'
-// 	}
-// }
+const Inspector = styled.a`
+	color: #fafafa;
+	width: 204px;
+	background-color: #92374d;
+	z-index: 2;
+	position: fixed;
+	text-decoration: none;
+	text-align: center;
+	padding: 6px 10px;
+	top: 3px;
+	left: 3px;
+	font-size: 15px;
+`
 
 export class App extends Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			status: 'inspector',
-		}
-	}
-
-	setStatus = () => {
-		if (this.state.status === 'inspector') {
-			this.setState({ status: 'view' })
-		} else {
-			this.setState({ status: 'inspector' })
-		}
-	}
-
 	render = () => (
 		<div style={{ position: 'relative', width: '100%', height: '100%' }}>
-			{this.state.status === 'inspector' ? (
-				<button
-					style={{
-						width: '100px',
-						height: '50px',
-						position: 'absolute',
-						zIndex: '2',
-					}}
-					onClick={this.setStatus}
-				>
-					Inspector
-				</button>
-			) : (
-				<button
-					style={{
-						width: '100px',
-						height: '50px',
-						position: 'absolute',
-						zIndex: '2',
-					}}
-					onClick={this.setStatus}
-				>
-					View
-				</button>
-			)}
+			<Inspector href="javascript:window.postMessage('INJECT_AFRAME_INSPECTOR','*')">
+				Inspect Scene
+			</Inspector>
 			<div>
 				<ExamplePage style={{ zIndex: '1' }} />
 			</div>
 		</div>
 	)
 }
-
-// const App = () => (
-// 	// <Switch>
-// 	// 	<Route exact path="/" component={ExamplePage} />
-// 	// </Switch>
-// <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-// 	{ console.log(state.status) }
-// 	<button style={{ width: '100px', height: '50px', position: 'absolute', zIndex: '2' }} onClick={setStatus}>{state.status}</button>
-// 	<div>
-// 	{/* <ExamplePage style={{ zIndex: '1' }} /> */}
-// 	</div>
-// </div>
-// )
 
 export default App
