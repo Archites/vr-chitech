d6143c404f4f9223e60b9008a241efff5b372775
Narongsak Chobsri <narongsak.chobsri@gmail.com>
2019-02-02 17:48:42 +0700

    Update index.js

--- src/pages/ExamplePage/index.js
+++ src/pages/ExamplePage/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const ExamplePage = () => (
-	<a-scene>
-		<a-entity
-			io3d-furniture="id:10344b13-d981-47a0-90ac-f048ee2780a6"
-			position="-2 0 -3.2"
-			rotation="0 180 0"
-		/>
-	</a-scene>
+  <a-scene>
+    <a-entity
+      io3d-furniture="id:10344b13-d981-47a0-90ac-f048ee2780a6"
+      position="-2 0 -3.2"
+      rotation="0 180 0"
+    />
+  </a-scene>
 )
 
 export default ExamplePage
