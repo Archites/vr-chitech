f36ae5dd77dce016965a5cacaf67cbd22c4c2aa9
babestvl <babestvl@gmail.com>
2019-04-21 16:11:47 +0700

    Remove floor ceiling

--- src/pages/SavePage/defaultElement.js
+++ src/pages/SavePage/defaultElement.js
@@ -1,8 +1,8 @@
 export default `
-  <Entity id="floor1" io3d-floor position="0 0 0" />
-  <Entity id="floor2" io3d-floor position="-4 0 0" />
-  <Entity id="floor3" io3d-floor position="0 0 -4" />
-  <Entity id="floor4" io3d-floor position="-4 0 -4" />
+  <Entity id="floor1" io3d-floor="hasCeiling: false" position="0 0 0" />
+  <Entity id="floor2" io3d-floor="hasCeiling: false" position="-4 0 0" />
+  <Entity id="floor3" io3d-floor="hasCeiling: false" position="0 0 -4" />
+  <Entity id="floor4" io3d-floor="hasCeiling: false" position="-4 0 -4" />
   <Entity id="wall1" io3d-wall position="-4 0 -4" rotation="0 0 0" />
   <Entity id="wall2" io3d-wall position="-3 0 -4" rotation="0 0 0" />
   <Entity id="wall3" io3d-wall position="-2 0 -4" rotation="0 0 0" />
@@ -11,7 +11,7 @@ export default `
   <Entity id="wall6" io3d-wall position="1 0 -4" rotation="0 0 0" />
   <Entity id="wall7" io3d-wall position="2 0 -4" rotation="0 0 0" />
   <Entity id="wall8" io3d-wall position="3 0 -4" rotation="0 0 0" />
-  <Entity id="wall9"io3d-wall position="-4 0 1" rotation="0 90 0" />
+  <Entity id="wall9" io3d-wall position="-4 0 1" rotation="0 90 0" />
   <Entity id="wall10" io3d-wall position="-4 0 2" rotation="0 90 0" />
   <Entity id="wall11" io3d-wall position="-4 0 3" rotation="0 90 0" />
   <Entity id="wall12" io3d-wall position="-4 0 4" rotation="0 90 0" />
